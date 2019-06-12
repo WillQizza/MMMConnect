3bd5ab0fca7a0ed25f68f756bebf8466cf865a76
WillQi <williamqipizza@gmail.com>
2019-06-12 07:40:50 -0600

    markdown works for bolding text

--- assets/js/markdown.js
+++ assets/js/markdown.js
@@ -10,9 +10,21 @@
  */
 const markdownify = text => {
     let soFar = "";
-    let pair = "";
     for (let i = 0; i < text.length; i++) {
-        pair += text[i];
+        if (text[i] === "*" && text[i + 1] === "*") {
+            // Where's the next closing tag?
+            const closingTag = text.substring(i + 2).indexOf("**");
+            if (closingTag !== -1) {
+                const message = text.substring(i + 2, closingTag + i + 2);
+                soFar += `<b>${message}</b>`;
+                text = text.substring(0, i) + message + text.substring(closingTag + i + 4);
+                i += message.length - 1;
+            } else {
+                soFar += text.substring(i, i + 2);
+            }
+        } else {
+            soFar += text[i];
+        }
     }
     return soFar;
 };
